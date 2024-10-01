24fd221677d12f6bf5752ace31bf181a543ea226
vinodpanzade <vinodpanzade64@gmail.com>
2024-10-01 22:34:20 +0530

    Resolved conflict in README.md

---
+++ src/components/Projects.js
@@ -0,0 +1,98 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Mern Book Portal",
+      description: "Web development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Hotel Managment",
+      description: "Frontend Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "AI Image Generator",
+      description: "AI and Web Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Amazon Clone",
+      description: "Frontend Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "PDF to data Convertor",
+      description: "AI ml and web development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Simple News Website",
+      description: "Frontend Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>I developed a MERN Book Portal with user authentication and a PDF to Data Converter. I also built web development projects using React and Bootstrap.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                     
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
